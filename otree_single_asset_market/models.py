05b626bd016d996b66fc2089129d9264f5ab8226
Tom Bowen <31299263+Tcbowen@users.noreply.github.com>
2020-10-23 15:24:26 -0700

    added survey and cleaned up html

--- otree_single_asset_market/models.py
+++ otree_single_asset_market/models.py
@@ -11,7 +11,8 @@ import numpy as np
 class Constants(BaseConstants):
     name_in_url = 'otree_single_asset_market'
     players_per_group = None
-    num_rounds = 99 
+    num_rounds = 20 
+
 
     # the columns of the config CSV and their types
     # this dict is used by ConfigManager
@@ -36,36 +37,32 @@ class Subsession(markets_models.Subsession):
     def creating_session(self):
         ##################################
         #signal code
-        #######################################
+        ##################################
         for player in self.get_players():
-
-            ## set the world state prob =.5
+            ## set the world state good or bad at probabilty of .5
             world_state_binomial = np.random.binomial(1, 0.5)
             player.world_state = world_state_binomial
-            
+            ##good state
             if player.world_state == 1:
                 if player.signal_nature==1:
-                    ##good = 4 black
+                    ##hi = 4 black
                     signal1_blackballs = 4
                     
                 else:  
-                    ##weak
-                    ##bad = 2 black
+                    ##low = 2 black
                     signal1_blackballs = 3               
-             
+            ##bad state
             if player.world_state == 0:
                 if player.signal_nature==1:
-                    ##good = 3 black
+                    ##hi = 3 black
                     signal1_blackballs = 1
                 else:  
-                    ##weak
-                    ##bad = 2 black
-                    signal1_blackballs = 2
-             
-            ### Choose two ball from the signal...
-            player.signal1_black = np.random.binomial(2,signal1_blackballs/4) ### is this correct
+                    ##low = 2 black
+                    signal1_blackballs = 2  
+            ### cacluated the black balls to be shown using binomial distribution
+            player.signal1_black = np.random.binomial(2,signal1_blackballs/5) 
+            ## white balles = 2-black
             player.signal1_white = 2-player.signal1_black
-
         if self.round_number > self.config.num_rounds:
             return
         return super().creating_session()
@@ -91,3 +88,31 @@ class Player(markets_models.Player):
     signal1_white = models.IntegerField()
 
     signal_nature = models.IntegerField(initial=1)
+
+    Question_1 = models.IntegerField(
+        label='''
+        After the trading, you should have a better idea of what is the true state of the wolrd in this
+        this trading period. Please use the infromation to the right hand side to answer the 
+        following question truthfully. 
+
+        What is the probailty( out of 100) that you believe the true state is 'G'?
+        Your answer:'''
+    )
+
+    Question_2 = models.IntegerField(
+        label='''
+        Please provide an interval that you are 87.5 onfident that the computer's 
+        prediction of the true state is 'G' falls into. Again please answer the queastion
+        truthfully.
+
+        your answer:
+        '''
+    )
+
+    Question_3 = models.IntegerField(
+    	choices=[1,2,3,4,5,6,7,8],
+        label='''
+        How would you rank your own performance in this trading period? in another worlds, what is the beleive of your own
+        ranking of your profit in this period. Please choose one of the following. 
+        '''
+    )
--- otree_single_asset_market/pages.py
+++ otree_single_asset_market/pages.py
@@ -1,10 +1,15 @@
 from otree_markets.pages import BaseMarketPage
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from ._builtin import Page, WaitPage
 
 class Market(BaseMarketPage):
+    timeout_seconds = 10
+    def before_next_page(self):
+        if self.timeout_happened:
+            self.player.save()
 
-    def is_displayed(self):
-        return self.round_number <= self.subsession.config.num_rounds
+    #def is_displayed(self):
+       # return self.round_number <= self.subsession.config.num_rounds
 
     def vars_for_template(self):
         ##load signal
@@ -20,4 +25,31 @@ class Market(BaseMarketPage):
             'img_sig_url': img_sig_url,
         }
 
-page_sequence = [Market]
+class Survey(Page):
+    def vars_for_template(self):
+            ##load signal
+            def before_next_page(self):
+                self.player.save()
+            #def is_displayed(self):
+              #  return self.round_number <= self.subsession.config.num_rounds
+
+            img_sig_url = static(
+                'otree_single_asset_market/signal_{}.jpg'.format(self.player.signal_nature))
+            ## load balls
+            img_url = static(
+                'otree_single_asset_market/balls2/balls_{}.jpg'.format(self.player.signal1_black))
+            return {
+                'signal1black': self.player.signal1_black,
+                'signal1white': self.player.signal1_white,
+                'img_url': img_url,
+                'img_sig_url': img_sig_url,
+            }
+
+    form_model = 'player'
+    form_fields = ['Question_1', 'Question_2', 'Question_3']
+
+class Wait(WaitPage):
+    pass
+
+
+page_sequence = [Market, Survey, Wait]

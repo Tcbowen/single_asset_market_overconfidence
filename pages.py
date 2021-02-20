3d7d2ee9c54a54d0110ff93488e4f15f9d2a5599
Tom Bowen <tcbowen@ucsc.edu>
2021-02-19 18:54:51 -0800

    added new templates for resuls page

--- pages.py
+++ pages.py
@@ -91,13 +91,48 @@ class Wait(WaitPage):
     wait_for_all_groups = True
     
     after_all_players_arrive = 'set_payoffs'
-    
-class Results(Page):
+class Results_state(Page):
+    def get_timeout_seconds(self):
+        return 10
+    def vars_for_template(self): 
+        if self.player.world_state==1:
+            state="G"
+        elif self.player.world_state==0:
+            state="B"
+
+        return {
+        'shares': self.player.shares,
+        'state': state,
+        }
+class Results_trading(Page):
+    def get_timeout_seconds(self):
+        return 10
+    def vars_for_template(self): 
+        return{
+            'profit': self.player.profit,
+            'asset_value': self.player.asset_value,
+            'cash_flow': self.player.settled_cash,
+            'payoff_from_trading': self.player.payoff_from_trading,
+        }
+class Results_survey(Page):
+    def get_timeout_seconds(self):
+        return 10
+    def vars_for_template(self): 
+        return{
+            'Question_1_pay_post': self.player.Question_1_payoff_post,
+            'Question_2_pay_post': self.player.Question_2_payoff_post,
+            'Question_3_pay_post': self.player.Question_3_payoff_post,
+            'Question_1_pay_pre': self.player.Question_1_payoff_pre,
+            'Question_2_pay_pre': self.player.Question_2_payoff_pre,
+            'Question_3_pay_pre': self.player.Question_3_payoff_pre,
+            'payoff_from_survey': self.player.survey_avg_pay, 
+        }
+class Results_total(Page):
     def get_timeout_seconds(self):
         if self.subsession.round_number==2:
             return 1000
         else:
-            return 30
+            return 15
     def before_next_page(self):
         if self.timeout_happened:
             self.player.save()
@@ -126,4 +161,4 @@ class Results(Page):
         }
 
 
-page_sequence = [Pre_Trading_Survey, Wait_for_trading, Market, Post_Trading_Survey, Wait, Results]
+page_sequence = [Wait_for_trading, Pre_Trading_Survey, Wait_for_trading, Market, Post_Trading_Survey, Wait, Results_state, Results_trading, Results_survey, Results_total]
